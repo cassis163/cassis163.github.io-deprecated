fb7775052a439a76b6f8698e57b7534cc5a5502b
cassis163 <cas.aangeenbrug@gmail.com>
2021-11-05 11:18:27 +0100

    Add Sass setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'pages'),
+      path.join(__dirname, 'component'),
+    ],
+  },
 }
