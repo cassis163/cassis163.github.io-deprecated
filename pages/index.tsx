cccc84895c4a8c72525a5f69f6cfd6c84c455f4c
cassis163 <cas.aangeenbrug@gmail.com>
2021-11-09 15:18:40 +0100

    Add chess to description

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const getAge = (birthDate: Date) => {
 const Home: NextPage = () => {
   const age = getAge(new Date("03/05/2001"));
   const text = `
-  My name is Casper Aangeenbrug. I am ${age.toString()} years old and I like programming, sports (mainly football), physics and maths.
+  My name is Casper Aangeenbrug. I am ${age.toString()} years old and I like programming, sports (mainly football and chess), physics and maths.
   At the moment, I am studying HBO-ICT at the HZ University of Applied Sciences.
   The study track that I chose is 'Software Engineering'.
   The alternatives were 'Business Consultancy' and 'Data Science'.
